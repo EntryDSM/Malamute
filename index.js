03c94e963f6601fd5cc716d2429323238ae6e900
huewilliams <4879sinwoo@naver.com>
2020-09-17 10:24:36 +0900

    FIX promise link

--- index.js
+++ index.js
@@ -3,53 +3,35 @@ const sharp = require('sharp');
 
 const s3 = new aws.S3({region: 'ap-northeast-2'});
 
-exports.handler = async (event, context, callback) => {
+exports.handler = (event, context, callback) => {
     const bucket = event.Records[0].s3.bucket.name;
     const key = event.Records[0].s3.object.key;
     const ext = key.split('.')[1];
     const filename = key.split('/')[1];
-    
-    try {
-        const s3Object = await s3.getObject({
-            Bucket: bucket,
-            Key: key
-        })
-        .promise()
-        .then(data => sharp(data.Body)
-            .resize(113, 151)
-            .toFormat(ext, { quality: 75})
-            .withMetadata()
-            .toBuffer()
-        );
 
-        s3.putObject({
+    s3.getObject({
+        Bucket: bucket,
+        Key: key
+    })
+    .promise()
+    .then(data => sharp(data.Body)
+        .resize(113, 136)
+        .toFormat(ext, { quality: 75})
+        .withMetadata()
+        .toBuffer()
+    )
+    .then(buffer => s3.putObject({
             Bucket: bucket,
             Key: `${filename}`,
-            Body: s3Object,
-        })
-        .promise()
-        .then(err => {
-            if (err) {
-                console.error(err);
-                return callback(err);
-            }
-        })
+            Body: buffer
+        }).promise()
+    )
+    .then(() => s3.deleteObject({
+        Bucket: bucket,
+        Key: key
+        }).promise()
+    )
+    .then(() => callback(null, `${key}`))
+    .catch(err => callback(err))
 
-        s3.deleteObject({
-            Bucket: bucket,
-            Key: key
-        })
-        .promise()
-        .then(err => {
-            if(err) {
-                console.error(err);
-                return callback(err);
-            }
-            return callback(null, `${key}`);
-        })
-
-    } catch (err) {
-        console.error(err);
-        return callback(err);
-    }
 }
